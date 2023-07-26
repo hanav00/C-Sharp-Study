ad1e10680fc4f72f972a0e8b48de596bfc5a551f
HanaV <123611673+hanav00@users.noreply.github.com>
2023-07-27 07:35:29 +0900

    Add files via upload

---
+++ 01/HelloWorld/HelloWorld.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine(args.Length);
+
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Hello " + args[0]);
+
+                // Check the type of args[0]
+                if (args[0].GetType() == typeof(string))
+                {
+                    Console.WriteLine("args[0] is of type string.");
+                }
+                else if (args[0].GetType() == typeof(int))
+                {
+                    Console.WriteLine("args[0] is of type int.");
+                }
+                else
+                {
+                    Console.WriteLine("args[0] is of an unknown type.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No command-line arguments provided.");
+            }
+        }
+    }
+}
