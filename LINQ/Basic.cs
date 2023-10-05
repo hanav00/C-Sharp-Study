ee0c0a41754ff3aa619ea016bb4c79218634787d
HanaV <123611673+hanav00@users.noreply.github.com>
2023-10-05 11:48:14 +0900

    Create Basic.cs

---
+++ LINQ/Basic.cs
@@ -0,0 +1,100 @@
+using System.Data;
+
+namespace LinqPractice
+{
+    class MainApp
+    {
+        class Inventory
+        {
+            public string? Product { get; set; }
+            public int Count { get; set; }
+        }
+
+        class Goods
+        {
+            public string? Product { get; set; }
+            public int Price { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            // 1. LINQ Basic (from, where, orderby, select)
+            int[] numbers = { 9, 2, 8, 7, 6, 5, 4, 3, 2, 11 };
+            var result = from n in numbers
+                         where n % 2 == 0
+                         orderby n descending
+                         select n;
+            foreach (var n in result) {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine();
+
+            var result2 = numbers.Where(n => n % 2 == 0)
+                                 .OrderByDescending(n => n)
+                                 .Select(n => n);
+            foreach (var n in result2) {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine();
+
+            // 1-2. select new()
+            Inventory[] fruitStore =
+            {
+                new Inventory(){Product="Grape", Count=8},
+                new Inventory(){Product="Pear", Count=5},
+                new Inventory(){Product="Strawberry", Count=9},
+                new Inventory(){Product="Blueberry", Count=50},
+                new Inventory(){Product="Persimmon", Count=14},
+                new Inventory(){Product="Apple", Count=3}
+            };
+            var fruitCheck = from fruits in fruitStore
+                             where fruits.Count < 10
+                             orderby fruits.Product
+                             select new
+                             {
+                                 Name = fruits.Product,
+                                 Required = 10 - fruits.Count
+                             };
+            foreach (var fruit in fruitCheck){
+                Console.WriteLine($"{fruit.Name} : + {fruit.Required}");
+            }
+            Console.WriteLine();
+
+            var fruitCheck2 = fruitStore.Where(fruits => fruits.Count < 10)
+                                        .OrderBy(fruits => fruits.Product)
+                                        .Select(fruits => new
+                                        {
+                                            Name = fruits.Product,
+                                            Required = 10 - fruits.Count
+                                        });
+            foreach (var fruit in fruitCheck2) {
+                Console.WriteLine($"{fruit.Name} : + {fruit.Required}");
+            }
+            Console.WriteLine();
+
+            // 1-3. join
+            Goods[] fruitSeller =
+            {
+                new Goods(){Product="Grape", Price=5000},
+                new Goods(){Product="Pear", Price=7000},
+                new Goods(){Product="Strawberry", Price=3000},
+                new Goods(){Product="Blueberry", Price=1000},
+                new Goods(){Product="Persimmon", Price=4000},
+                new Goods(){Product="Apple", Price=3500}
+            };
+            var fruitPrice = from store in fruitStore
+                             join seller in fruitSeller on store.Product equals seller.Product
+                             where store.Product.Contains('r') && seller.Price <= 5000
+                             orderby store.Product
+                             select new
+                             {
+                                 Name = store.Product,
+                                 RequiredCost = seller.Price * store.Count
+                             };
+            foreach (var fruit in fruitPrice) {
+                Console.WriteLine($"{fruit.Name} ->  {fruit.RequiredCost} Won");
+            }
+            Console.WriteLine();
+        }
+    }
+}
